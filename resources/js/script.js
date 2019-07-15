043aa427d217e036f456065b2d3a24bf43916557
aaron mayhew <aaronmayhew@aarons-MBP-2.attlocal.net>
2019-07-15 01:49:30 -0500

    updated media queries for sticky nav

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,6 +64,23 @@ $(document).ready(function() {
         offset: '50%'
     });
     
+/* mobile nav */
+    $('.js--nav-icon').click(function() {
+       var nav = $('.js--main-nav');
+       var icon = $('.js--nav-icon i');
+       
+       nav.slideToggle(200);
+       
+       if (icon.hasClass('ion-navicon-round')) {
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+       } else {
+           icon.addClass('ion-navicon-round');
+           icont.removeClass('ion-close-round');
+       }
+    });
+    
+    
     
     
     
