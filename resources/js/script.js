2d231c96af6e0d5485a1741d811ec582fd209dcd
aaron mayhew <aaronmayhew@aarons-MBP-2.attlocal.net>
2019-07-14 19:59:03 -0500

    adding more animations via jquery

--- resources/js/script.js
+++ resources/js/script.js
@@ -23,6 +23,7 @@ $(document).ready(function() {
     });
     
 /* navigation scroll */
+    
     $(function() {
         $('a[href*="#"]:not([href="#"])').click(function() {
             if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -37,4 +38,33 @@ $(document).ready(function() {
             }
         });
     });
+    
+/* animations on scroll */
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2').waypoint(function(direction) {
+        $('.js--wp-2').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
+    
+    
+    
+    
 });
